304c32ab8a5daa446a5b6860a0c1e299f06a5fe5
Tawanda Jaure <tawandajaujau@gmail.com>
2022-01-08 16:18:27 +0200

    Handle game over scenario
    > Create and export getSnakeHead() from snake.js
    > Create and export snakeIntersection() from snake.js -
      calls onSnake() on snake head to check if the snake
      has eaten itself
    * Ensure onSnake does not iterate over the head,
      otherwise onSnake will always return true
    > Create and export outsideGrid(position) from grid.js -
      checks if passed in position is outside grid
    > Create checkDeath() in game.js - returns game over
      status
    > Check game over status and restart game inside
      main() function

--- game.js
+++ game.js
@@ -1,15 +1,26 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js';
 import { update as updateFood, draw as drawFood } from './food.js';
+import { outsideGrid } from './grid.js';
 
 /* Set up game speed -
 how many times snake moves per second */
 let lastRenderTime = 0;
+
+// Initialize game over state
+let gameOver = false;
 
 // Get gameBoard Element
 const gameBoard = document.querySelector('#game-board');
 
 // Set up a game loop
 function main(currentTime) {
+  if (gameOver) {
+    if(confirm('You lost. Press OK to restart.')) {
+      window.location = '/';
+    }
+    return;
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < (1 / SNAKE_SPEED)) return;
@@ -24,10 +35,15 @@ window.requestAnimationFrame(main)
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = '';
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
